cb0eb34117763bc70f0ef6695919aeac411d3a61
Adam <58134265+AdamRoyal@users.noreply.github.com>
2021-01-20 01:51:00 -0700

    Update ChangeBuzzTone.cpp

--- ChangeBuzzTone.cpp
+++ ChangeBuzzTone.cpp
@@ -11,7 +11,7 @@ Serial.begin(9600);
 
 void loop() {
 potVal = analogRead(potPin);
-// point slope
+// Point Slope
 toneVal = (9940./1023.) * potVal + 60;
 digitalWrite(buzzPin, HIGH);
 delayMicroseconds(toneVal);
